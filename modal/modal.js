165f95a6bdb12f898f1f1dd57e6c9a20d883c2a7
Pietro Marino <pietro.marino_84@hotmail.it>
2019-12-30 11:20:54 +0100

    aggiunte alla modale animazioni fade per tutti i lati dello schermo

--- modal/modal.js
+++ modal/modal.js
@@ -1,22 +1,37 @@
 var opener = document.getElementById('opener');
-var modal = document.getElementById('modal');
-var modalOverlay = modal.querySelector('.modal-overlay');
+var modalWrapper = document.getElementById('modal');
+var modalOverlay = modalWrapper.querySelector('.modal-overlay');
+var modal = modalWrapper.querySelector('.modal');
 var closer = modal.querySelector('.modal__btn-closer');
 var btnSuccess = modal.querySelector('.btn-success');
 var btnCancel = modal.querySelector('.btn-cancel');
 
-const isModalOpened = () => modal.classList.contains('open');
+const isModalOpened = () => modalWrapper.classList.contains('open');
 
-function openModal() {
-  if(!isModalOpened()) {
-    modal.classList.add('open');
-  }
+function openModal(e) {
+  setTimeout(() => {
+    e.preventDefault();
+    if (!isModalOpened()) {
+      modalWrapper.classList.add('open');
+      closer.tabindex = 1;
+      btnCancel.tabindex = 0;
+      btnSuccess.tabindex = 3;
+      btnCancel.focus();
+    }
+  });
 }
 
-function closeModal() {
-  if(isModalOpened()) {
-    modal.classList.remove('open');
-  }
+function closeModal(e) {
+  setTimeout(() => {
+    e.preventDefault();
+    if (isModalOpened()) {
+      modalWrapper.classList.remove('open');
+      closer.removeAttribute("tabindex");
+      btnCancel.removeAttribute("tabindex");
+      btnSuccess.removeAttribute("tabindex");
+      btnCancel.blur();
+    }
+  });
 }
 
 opener.addEventListener('click', openModal);
